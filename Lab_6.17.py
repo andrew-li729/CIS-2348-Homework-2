854661a515e6215e6d206bcc1c07272b0bcaf9f3
Andrew <andrew.li729@gmail.com>
2020-10-06 15:53:46 -0500

    first try

--- Lab_6.17.py
+++ Lab_6.17.py
@@ -1,2 +1,16 @@
 # Andrew Li
 # 1824794
+
+password = input()
+password_key = {"i": "1",
+                "a": "@",
+                "m": "M",
+                "B": "8",
+                "o": "."}
+
+for element in password:
+    for key in password_key:
+        if key == element:
+            password = password.replace(element, password_key[key]) + 'q*s'
+
+print(password)
