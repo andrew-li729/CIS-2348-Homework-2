8f484de1caa84aa94b9627fd988f675d893e0444
Andrew <andrew.li729@gmail.com>
2020-10-06 14:44:19 -0500

    corrected last commit, part A not complete, working on part B and C

--- coding_problem_2.py
+++ coding_problem_2.py
@@ -7,6 +7,6 @@ for line in file:
         if line.find(month) != -1:
             month_num = months_list.index(month) + 1  # counts which month the loop is on
             remove_comma = line.replace(",", "")  # replaces comma in date with nothing
-            add_slash = remove_comma.replace(" ", "/") # replaces spaces with "/"
+            add_slash = remove_comma.replace(" ", "/")  # replaces spaces with "/"
             replace_month = add_slash.replace(month, str(month_num))  # replaces month name with month number
             print(replace_month)
