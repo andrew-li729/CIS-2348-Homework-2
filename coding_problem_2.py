4c0850ad1f9a2ac793eb80762aa56dd4693ca24a
Andrew <andrew.li729@gmail.com>
2020-10-06 14:38:59 -0500

    part A complete, prints correct dates in specified format

--- coding_problem_2.py
+++ coding_problem_2.py
@@ -1,7 +1,12 @@
 file = open("inputDates.txt", "r")
 months_list = ["January", "February", "March", "April", "May", "June", "July",
                "August", "September", "October", "November", "December"]
+
 for line in file:
-    for x in months_list:
-        if line.find(x) != -1:
-            print(line)
+    for month in months_list:
+        if line.find(month) != -1:
+            month_num = months_list.index(month) + 1  # counts which month the loop is on
+            remove_comma = line.replace(",", "")  # replaces comma in date with nothing
+            add_slash = remove_comma.replace(" ", "/") # replaces spaces with "/"
+            replace_month = add_slash.replace(month, str(month_num))  # replaces month name with month number
+            print(replace_month)
