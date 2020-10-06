c03bcfaa6fd1506ad267e317560a967399d908b9
Andrew <andrew.li729@gmail.com>
2020-10-06 17:04:32 -0500

    incorporated "no solution" check

--- Lab_6.22.py
+++ Lab_6.22.py
@@ -3,17 +3,27 @@
 x = 0
 y = 0
 
+a = int(input())  # var for 1st equation
+b = int(input())
+c = int(input())
+
+d = int(input())  # var for 2nd equation
+e = int(input())
+f = int(input())
+
+solution_x = 1000  # initializes final values for x and y, if true values are found below, they will be changed
+solution_y = 1000
+
 for x in range(-10, 11):
     for y in range(-10, 11):
-        equation1 = 8 * x + 7 * y
-        equation2 = 3 * x - 5 * y
-        if equation1 == 38 and equation2 == -1:
-            solution_x = x
+        equation1 = a * x + b * y  # ax + by
+        equation2 = d * x + e * y  # dx - ey
+        if equation1 == c and equation2 == f:
+            solution_x = x  # changes previously set values to solution
             solution_y = y
-            print(solution_x, solution_y)
 
-equation1 = 8 * solution_x + 7 * solution_y
-equation2 = 3 * solution_x - 5 * solution_y
 
-if equation1 != 38 and equation2 != -1:
+if solution_x != 1000 and solution_y != 1000:  # if the values were changed, meaning the equations were solved,
+    print(solution_x, solution_y)              # print x and y, else print no solution since they were not changed
+else:                                          # meaning no solution was found
     print("No solution")
