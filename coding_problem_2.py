e7e0f22365cfb627e31a215bd3f3aed4a03127ea
Andrew <andrew.li729@gmail.com>
2020-10-06 13:41:54 -0500

    prints dates that are formatted correctly

--- coding_problem_2.py
+++ coding_problem_2.py
@@ -1,3 +1,7 @@
 file = open("inputDates.txt", "r")
+months_list = ["January", "February", "March", "April", "May", "June", "July",
+               "August", "September", "October", "November", "December"]
 for line in file:
-    print(line)
+    for x in months_list:
+        if line.find(x) != -1:
+            print(line)
