1b79c2eb10a1eb934edd6a2f3e999fde1ad2e4a9
Andrew <andrew.li729@gmail.com>
2020-10-06 20:03:12 -0500

    palindrome check added

--- Lab_8.10.py
+++ Lab_8.10.py
@@ -1,3 +1,13 @@
 # Andrew Li
 # 1824794
 
+string_input = input()
+no_spaces = string_input.replace(" ", "")
+reversed_string = no_spaces[::-1]
+
+if reversed_string == no_spaces:
+    print(string_input, "is a palindrome")
+else:
+    print(string_input, "is not a palindrome")
+
+
