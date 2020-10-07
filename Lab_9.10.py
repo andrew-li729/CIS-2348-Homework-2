7d1cb7bdf449e4150fd9a0b786b5a2f7f51d8c3f
Andrew <andrew.li729@gmail.com>
2020-10-06 21:00:12 -0500

    removed extra print statements

--- Lab_9.10.py
+++ Lab_9.10.py
@@ -8,11 +8,11 @@ with open("input1.csv", 'r') as file:  # reads csv file and creates words list f
         words = row
 
 words2 = []
-print(words)
+
 
 for i in words:
     if i not in words2:  # makes a list of unique words in words list
         words2.append(i)
-print(words2)
+
 for i in range(0, len(words2)):  # iterates through new list
     print(words2[i], words.count(words2[i]))  # counts words on 1st word list based on unique word list
